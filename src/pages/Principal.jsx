477446c63313bff2e3b4096a53bf5f51d9355a77
Lucas <lucasmilessi98@gmail.com>
2023-03-02 11:45:23 -0300

    Creación de proyecto y distribución de carpetas

---
+++ src/pages/Principal.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../style/pages/principal.css'
+
+const URL_API = process.env.REACT_APP_API_URL_1;
+
+const Principal = () => {
+
+
+
+  return (
+    <div id='body'>
+        <header id='head'>
+            <nav id='nav'>
+
+            </nav>
+        </header>
+        <main id='main'>
+
+        </main>
+        <footer id='footer'>
+
+        </footer>
+    </div>
+  )
+}
+
+export default Principal;
